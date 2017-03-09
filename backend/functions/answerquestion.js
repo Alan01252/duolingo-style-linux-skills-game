11eea771e941b66b2aecfe037849ec515aa30c4e
Alan Hollis <me@alanhollis.com>
2017-03-09 16:42:21 +0000

    Fixes to work with AWS runtime

--- backend/functions/answerquestion.js
+++ backend/functions/answerquestion.js
@@ -5,15 +5,25 @@ const QuestionTester = require('../modules/questiontester');
 module.exports.answerquestion = (event, context, callback) => {
 
     let response = {
-        statusCode: 200
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        }
     };
 
-    console.log(context);
-    console.log(event);
-    let question = event.body;
+    let question = "";
+    /**
+     * When running on aws the event.body needs parsing, locally using webpack server
+     * this is being done automatically...
+     *
+     * TODO Investigate how to make webpack server *NOT* auto convert json
+     */
+    try {
+        question = JSON.parse(event.body);
+    } catch (ex) {
+        question = event.body
+    }
     question.correct = new QuestionTester().test(question);
-    console.log(new QuestionTester().test(question));
-    console.log(question.correct);
     question.rows.map((row, index) => {
         question.rows[index].found_matches = row.text.match(question.answer);
     });
--- backend/functions/getquestion.js
+++ backend/functions/getquestion.js
@@ -5,7 +5,10 @@ const QuestionGetter = require('../modules/questiongetter');
 module.exports.getquestion = (event, context, callback) => {
 
     let response = {
-        statusCode: 200
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        }
     };
 
     response.body = JSON.stringify(new QuestionGetter().get());
